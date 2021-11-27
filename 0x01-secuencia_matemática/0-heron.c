1100d68ff84063c2975121e00a3ceae7e08d8b8b
EdgarWill <84213405+EdgaWill@users.noreply.github.com>
2021-11-26 23:49:44 -0500

    Create 0-heron.c

---
+++ 0x01-secuencia_matemática/0-heron.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+typedef struct cell
+{
+	double elt;
+	struct cell *next;
+} t_cell;
+
+void push(t_cell **head_ref, double x)
+{
+	t_cell *new_node = malloc(sizeof(t_cell));
+
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return;
+	}
+
+	new_node->elt  = x;
+
+	new_node->next = (*head_ref);
+
+	(*head_ref)    = new_node;
+}
+
+t_cell *heron(double p, double x0)
+{
+	t_cell *h;
+	double error = 1 / 10000000;
+	double x = x0;
+
+	h = malloc(sizeof(t_cell));
+
+	if (h == NULL)
+		return (NULL);
+
+	h->elt = x;
+
+	while (fabs(p - (x * x)) > error)
+	{
+		if (x != x0)
+			push(&h, x);
+
+		x = 0.5 * (x + p / x);
+
+	}
+
+	return (h);
+}
