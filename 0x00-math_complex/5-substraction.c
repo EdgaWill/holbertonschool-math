869cc9ef080f7b1aba72da98014aa7811e9ccd47
edgar <edgar-william@hotmail.com>
2021-11-28 04:34:32 +0000

    function that performs the substraction operation to complex numbers

---
+++ 0x00-math_complex/5-substraction.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * substraction - function that performs the substraction
+ * operation to complex numbers
+ * @c1: complex number
+ * @c2: complex number
+ * @c3: complex number
+ */
+void substraction(complex c1, complex c2, complex *c3)
+{
+	c3->re = c1.re - c2.re;
+	c3->im = c1.im - c2.im;
+}
