01a40092f6e4b58ac717799e3bde97f6d269be3b
edgar <edgar-william@hotmail.com>
2021-12-11 04:17:23 +0000

    the Simpson Method and use it to calculate

---
+++ 0x02-math_integrals_and_ode/1-simpson.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * simpson - simpson_method
+ * @a: double
+ * @b: double
+ * @steps: int
+ * Return: double
+ */
+
+double simpson(double a, double b, int steps)
+{
+	double su, ab, ac;
+	int i;
+
+	su = 0.0;
+	ac = (b - a) / steps;
+
+	for (i = 1; i <= steps - 1; i++)
+	{
+		ab = a + i * ac;
+		if (i % 2 == 0)
+		{
+			su += 2 * f(ab);
+		}
+		else
+		{
+			su += 4 * f(ab);
+		}
+	}
+	su += 1 * f(a) + f(b);
+	su = su * (ac / 3);
+
+	return (su);
+}
