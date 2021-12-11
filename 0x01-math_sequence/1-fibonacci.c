2f44956ed295bdd908245f0f41a88c8b1cc41e43
edgar <edgar-william@hotmail.com>
2021-12-11 04:41:21 +0000

    Fibonacci sequence is defined

--- 0x01-math_sequence/1-fibonacci.c
+++ 0x01-math_sequence/1-fibonacci.c
@@ -1,73 +1,42 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
+#include "fibonacci.h"
 
-typedef struct fibo
-{
-	int elt;
-	struct fibo *next;
-} t_cell;
-
-void push(t_cell **head_ref, double new_data)
-{
-	t_cell *new_node = malloc(sizeof(t_cell));
-
-	if (new_node == NULL)
-	{
-		free(new_node);
-		return;
-	}
-
-	new_node->elt  = new_data;
-
-	new_node->next = (*head_ref);
-
-	(*head_ref)    = new_node;
-}
-
-t_cell *Fibonnaci()
-{
-	t_cell *h;
-	int i = 2;
-	int x0 = 1;
-	int x1 = 1;
-	int x;
-
-	h = malloc(sizeof(t_cell));
-
-	if (h == NULL)
-	{
-		free(h);
-		return (NULL);
-	}
-
-	h->elt = x0;
-
-	push(&h, x1);
-
-	while (i < 20)
-	{
-		x = x0 + x1;
-		push(&h, x);
-		x0 = x1;
-		x1 = x;
-		i++;
-	}
-
-	return (h);
-}
-
+/**
+ * gold_number - nummber
+ * @head: linked list
+ *
+ * Return: number
+ */
 double gold_number(t_cell *head)
 {
-	double rtn;
-	double x0, x1;
+	double ab, ac;
 
-	x0 = head->elt;
-  
+	ab = head->elt;
 	head = head->next;
-	x1 = head->elt;
-  
-	rtn = x1 / x0;
+	ac = head->elt;
+	return (ab / ac);
+}
 
-	return (rtn);
+/**
+ * Fibonnaci - Fibonacci sequence is defined
+ *
+ * Return: number
+ */
+t_cell *Fibonnaci()
+{
+	t_cell *head = NULL, *new;
+	int ab = 0, ac = 1, cd = 0, de = 20;
+
+	while (lim)
+	{
+		ab = ac + cd;
+
+		new = malloc(sizeof(t_cell));
+		new->elt = ac;
+		new->next = head;
+		head = new;
+		ab = ac;
+		ac = ab;
+		de--;
+	}
+	return (head);
 }
